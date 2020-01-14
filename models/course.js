5eacf2ae17b96a0d4c4e530f8373fb3db0bcbe21
willy1669 <williamschinonso11@gmail.com>
2020-01-14 17:20:08 +0100

    create course model

---
+++ models/course.js
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const courseSchema = new Schema({
+    courseTitle: {
+        type: String,
+    },
+    duration: {
+        type: String,
+    },
+    courseModule: [{
+        type: ObjectId,
+        ref: 'CourseModule',
+    }],
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+export default Course;
