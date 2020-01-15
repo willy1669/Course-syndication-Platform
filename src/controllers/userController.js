37ab86a14e215854628fd38590a43db1d903577f
willy1669 <williamschinonso11@gmail.com>
2020-01-15 07:29:30 +0100

    added userSignup function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,19 +2,19 @@ import { signUp, login} from '../services/userService';
 
 //Define user sign up
 export const userSignUp = async (req, res) => {
-    try {
-        const data = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        phoneNumber: req.body.phoneNumber,
-        countryCode: req.body.countryCode,
+  try {
+    const data = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      phoneNumber: req.body.phoneNumber,
+      countryCode: req.body.countryCode,
     };
-      const user = await signUp(req, res, data);
-      return user;
-    } catch (exception) {
-      console.error(`Error: ${exception}`);
-    }
+    const user = await signUp(req, res, data);
+    return user;
+  } catch (exception) {
+    console.error(`Error: ${exception}`);
+  }
 };
 
 //Define user login
