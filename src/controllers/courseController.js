5478a20507526e6d16babc1b43b7837af1d7c235
willy1669 <williamschinonso11@gmail.com>
2020-01-16 16:12:35 +0100

    create getCourseByTitle function

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,12 +1,22 @@
-import { createCourse } from '../services/courseService';
+import { createCourse, getCourseByTite } from '../services/courseService';
 
 //Define create course function
 export const createNewCourse = async (req, res) => {
-    try {
-        const { courseTitle } = req.body;
-        const user = await createCourse(req, res, data);
-        return user;
-    } catch (exception) {
-      console.error(`Error: ${exception}`);
-    }
-  };
+  try {
+    const { courseTitle } = req.body;
+    const user = await createCourse(req, res, data);
+    return user;
+  } catch (exception) {
+    console.error(`Error: ${exception}`);
+  }
+};
+
+export const findACourse = async (req, res) => {
+  try {
+    const { courseTitle } = req.params;
+    const user = await createCourse(req, res, courseTitle);
+    return user;
+  } catch (exception) {
+    console.error(`Error: ${exception}`);
+  }
+};
--- src/services/courseService.js
+++ src/services/courseService.js
@@ -7,3 +7,11 @@ export const createCourse = async (req, res, data) => {
         res.status(202).json({ message: 'course created successfully' });
     }
 }
+
+export const getCourseByTite = async (req, res, courseTitle) => {
+    const courseResult = await course.findOne({ courseTitle: courseTile });
+    if (error) res.status(402).json({ message: 'error, something happened'});
+    else {
+        res.status(202).json(courseResult);
+    }
+}
